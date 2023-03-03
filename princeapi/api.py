4dd7bd49844a8e88f80e95db2c5c1b8900f369da
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-03 13:45:10 +0530

    Update api.py

--- princeapi/api.py
+++ princeapi/api.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def anime_logo(name):
-    API = f"https://api.princexd.tech/anime-logo?name={name}"
+def anime_logo(text):
+    API = f"https://api.princexd.tech/anime-logo?text={text}"
     req = requests.get(API).json()["url"]
     return(req)
 
 def write(text):
-    API = f"https://api.princexd.tech/write?text={name}"
+    API = f"https://api.princexd.tech/write?text={text}"
     req = requests.get(API).json()["url"]
     return(req)
