5fa87de35a8f644237076b9a23a7d660e6fc1dc2
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-03 13:39:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf8") as readme:
 setuptools.setup(
     name="prince-api",
     packages=setuptools.find_packages(),
-    version="0.0.4",
+    version="0.0.1",
     license="MIT",
     description="A Project Made To Centralize Various APIs 📖 No Authorization Needed :)",
     long_description=long_description,
